2155dabd129caf2614231e2c4bcb39b1171fdbc0
mparaiso <aikah@free.fr>
2013-03-21 10:41:02 +0100

    commit

--- app/SnippetManager.php
+++ app/SnippetManager.php
@@ -24,7 +24,7 @@ class SnippetManager extends Silex\Application
         $this->mount("/admin", new CategoryController);
         if($this["debug"]==FALSE){
             // force HTTPS on heroku
-            $this->after(function(Request $req,Response $resp,Application $app){
+            $this->after(function(Request $req,Response $resp){
                 $resp->headers->add(array("Strict-Transport-Security: max-age=31536000; includeSubDomains"));
                 return $resp;
             });
