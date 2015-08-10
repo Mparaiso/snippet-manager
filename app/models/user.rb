3df43ec8e7e342ba35d2ca84e7a61f61e1015be0
mparaiso <aikah@free.fr>
2015-08-10 10:01:52 +0000

    TODO3

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ActiveRecord::Base
   validates :email, presence: true, length:{minimum:7,maximum:255}
   validates :nickname,presence: true,length:{minimum:5,maximum:255}
 
+  #TODO write documentation for method
   def can_access_complete_credentials_of(user)
     return self.id == user.id
   end
